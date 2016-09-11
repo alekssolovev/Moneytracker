01cce176c1210444ab32813b6f8541331584180c
Aleksandr Solovev <solovey_alex1990@mail.ru>
2016-09-11 17:12:19 +0300

    Add onStop, onRestart and onDestroy methods.

--- app/src/main/java/com/example/sani4/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/sani4/moneytracker/MainActivity.java
@@ -35,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onPause()");
     }
 
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop()");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart()");
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy()");
+    }
 
 
 
