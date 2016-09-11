c72d60d91bc75655770e2ca0bf4b837a5353e05a
Aleksandr Solovev <solovey_alex1990@mail.ru>
2016-09-11 15:27:13 +0300

    Add onCreate method.

--- app/src/main/java/MainActivity.java
+++
@@ -1,8 +0,0 @@
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by sani4 on 11.09.16.
- */
-public class MainActivity extends AppCompatActivity {
-
-}
---
+++ app/src/main/java/com/example/sani4/moneytracker/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sani4.moneytracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by sani4 on 11.09.16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+    }
+
+
+
+}
