ada74586226641a33b242d41661cee7260f90e52
Aleksandr Solovev <solovey_alex1990@mail.ru>
2016-09-11 16:02:49 +0300

    Add onStart, onResume and onPause methods.

--- app/src/main/java/com/example/sani4/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/sani4/moneytracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sani4.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 
 /**
@@ -9,14 +10,33 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
+        Log.d(LOG_TAG, "onCreate()");
 
     }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart()");
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+    }
+
+
+
 
 
 
